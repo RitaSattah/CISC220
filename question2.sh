46bea1efd372d33509c6228cb76e7f9fe489f8e7
RitaSattah <toutiuni@hotmail.com>
2016-10-17 15:59:01 -0400

    Update question2.sh

--- question2.sh
+++ question2.sh
@@ -1,1 +1,20 @@
-Question 2
+#!/bin/bash
+
+
+ipa=$(ifconfig | grep -e "inet addr:" | awk '{print $2}' | awk -F ":" '{print $2}')
+
+
+for i in $ipa; do
+
+    if [[ $i == 127* ]]; then
+        echo "Local IP is:" $i ;
+    
+    elif [[ $i == 192.168* || $i == 10* ]] ; then
+        echo "Private IP is:" $i ;
+    
+    else
+        echo $i "Public IP" ;
+    
+    fi
+
+done
